a8a7b88b0fa11b8f1a1f25c8920b2efefca5f011
mosesmarin <moises.c.marin@gmail.com>
2019-04-03 11:55:10 -0700

    add consumer-parms

--- python-app05/consumer-parms.py
+++ python-app05/consumer-parms.py
@@ -1,20 +1,33 @@
+import sys
 from confluent_kafka import Consumer, KafkaError
 
+if len(sys.argv)<5:
 
+    print ("Usage:")
+    print ("python3 consumer-parms brokers topic user key ")
+    sys.exit()
 
-
+brokers=sys.argv[1]
+topic=sys.argv[2]
+user=sys.argv[3]
+key=sys.argv[4]
 
 
 print ("Starting consumer");
 
 # Start consumer
 c = Consumer({
-    'bootstrap.servers': 'mybroker',
+    'bootstrap.servers': '$brokers',
+    'sasl.mechanisms': 'SCRAM-SHA-512',
+    'security.protocol': 'SASL_SSL',
+    'sasl.username': '$user',
+    'sasl.password': '$key',
     'group.id': 'mygroup',
     'auto.offset.reset': 'earliest'
 })
 
-c.subscribe(['mytopic'])
+c.subscribe(['$topic'])
+print (c) 
 
 while True:
     msg = c.poll(1.0)
