837bb0e42b2197d23f6970f474fa7d4cc42c415b
mosesmarin <moises.c.marin@gmail.com>
2019-09-01 18:28:29 -0700

    send qr code

--- python-app06/install-requirements.sh
+++
@@ -1,9 +0,0 @@
-export LD_LIBRARY_PATH=/opt/rh/rh-python34/root/usr/lib64:/opt/rh/rh-nodejs6/root/usr/lib64:/opt/rh/httpd24/root/usr/lib64
-cd /opt/app-root/src/confluent-kafka-python
-python setup.py build
-python setup.py install
-
-cd /opt/app-root/src/kafkacat
-./configure
-make
-make install
